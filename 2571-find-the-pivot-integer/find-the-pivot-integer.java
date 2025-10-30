3c451372413b06951cbdc7463d5bb65b37fe2505
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-10-30 20:44:22 +0530

    Time: 1 ms (64.56%) | Memory: 41 MB (24.69%) - LeetSync

--- 2571-find-the-pivot-integer/find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/find-the-pivot-integer.java
@@ -1,31 +1,13 @@
 class Solution {
     public int pivotInteger(int n) {
-        int j=1;
-        if(n==1){
-            return 1;
-        }
-         while(j!=n){
+        int total = n * (n + 1) / 2;  // sum of 1 to n
 
-           int x=0;
-           j++;
-           int sum=0;
-           int sum1=0;
-        for(int i=1;i<=n;i++){
-            
-                if(j>=i){
-                    sum=sum+i;
-                    x=i;
-                }
-                else{
-                    sum1=sum1+i;
-                }
-                
+        for (int i = 1; i <= n; i++) {
+            int leftSum = i * (i + 1) / 2;
+            int rightSum = total - leftSum + i;
+            if (leftSum == rightSum)
+                return i;
         }
-        if(sum==(sum1+x)){
-            return x;
-        }
-
-    }
-    return -1;
+        return -1;
     }
 }
