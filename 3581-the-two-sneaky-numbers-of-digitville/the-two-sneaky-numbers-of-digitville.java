7d0db84331d0e7b606672b27c25c85720f2a5941
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-10-31 14:13:36 +0530

    Time: 2 ms (63.09%) | Memory: 46.8 MB (7.71%) - LeetSync

--- 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
+++ 3581-the-two-sneaky-numbers-of-digitville/the-two-sneaky-numbers-of-digitville.java
@@ -1,22 +1,20 @@
 class Solution {
     public int[] getSneakyNumbers(int[] nums) {
         HashSet<Integer> hs=new HashSet<>();
-         HashSet<Integer> hs1=new HashSet<>();
-
+         int arr[]=new int[2];
+         int j=0;
         for(int i=0;i<nums.length;i++){
           if(!hs.contains(nums[i])){
             hs.add(nums[i]);
           }
           else{
-            hs1.add(nums[i]);
+         arr[j]=nums[i];
+         j++;
           }
         }
-   int arr[]=new int[2];
-   int i=0;
-    for(int num:hs1){
-        arr[i]=num;
-        i++;
-    }
+   
+   
+   
        
        return arr;
     }
