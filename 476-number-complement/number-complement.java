9aa6f0e04e287334f7b474047206f11945e8f719
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-10-31 15:23:06 +0530

    Time: 1 ms (25.89%) | Memory: 42.3 MB (6.93%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,17 +1,10 @@
 class Solution {
     public int findComplement(int num) {
-        String binary="";
-        while(num>0){
-            int rem=num%2;
-            if(rem==0){
-                binary=1+binary;
-            }
-            else{
-                binary=0+binary;
-            }
-            num=num/2;
+        StringBuilder binary = new StringBuilder();
+        while (num > 0) {
+            binary.insert(0, (num % 2 == 0) ? '1' : '0');
+            num /= 2;
         }
-        int num1 = Integer.parseInt(binary, 2);
-        return num1;
+        return Integer.parseInt(binary.toString(), 2);
     }
 }
