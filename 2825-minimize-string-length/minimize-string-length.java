3bc1fb375035f7d1c9ea9b3318058c3693e6fc1d
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-12-11 18:02:55 +0530

    Time: 7 ms (72.51%) | Memory: 46.8 MB (33.86%) - LeetSync

---
+++ 2825-minimize-string-length/minimize-string-length.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int minimizedStringLength(String s) {
+        HashSet<Character> hs=new HashSet<>();
+        for(int i=0;i<s.length();i++){
+           if(!hs.contains(s.charAt(i))){
+            hs.add(s.charAt(i));
+           }
+        }
+        return hs.size();
+    }
+}
