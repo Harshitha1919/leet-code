214ac3aa16f45771a9d8812a8de46efcf79d5920
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-12-12 11:47:31 +0530

    Time: 3 ms (25.80%) | Memory: 43.8 MB (8.60%) - LeetSync

--- 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
+++ 2668-form-smallest-number-from-two-digit-arrays/form-smallest-number-from-two-digit-arrays.java
@@ -1,32 +1,35 @@
 class Solution {
     public int minNumber(int[] nums1, int[] nums2) {
-       
-        HashSet<Integer> hs1=new HashSet<>();
+        HashSet<Integer> hs = new HashSet<>();
+        HashSet<Integer> hs1 = new HashSet<>();
+
+        for (int x : nums1) hs.add(x);
+        for (int x : nums2) hs1.add(x);
+
+        // Find the smallest common number
+        int common = Integer.MAX_VALUE;
+
+        for (int x : nums1) {
+            if (hs1.contains(x)) {
+                common = Math.min(common, x);
+            }
+        }
+
+        for (int x : nums2) {
+            if (hs.contains(x)) {
+                common = Math.min(common, x);
+            }
+        }
+
+        if (common != Integer.MAX_VALUE) return common;
+
+        // No common digit → form smallest 2-digit number
         Arrays.sort(nums1);
         Arrays.sort(nums2);
-        
-         for(int i=0;i<nums2.length;i++){
-            hs1.add(nums2[i]);
-        }
-       
-    
-        for(int i=0;i<nums1.length;i++){
-           if(hs1.contains(nums1[i])){
-            return nums1[i];
-           }
-        }
-       
-        
-       
-        int h=nums1[0];
-        int l=nums2[0];
-        int ans=0;
-        if(h<l){
-         ans=h*10+l;
-        }
-        else{
-         ans=l*10+h;
-        }
-        return ans;
+
+        int a = nums1[0];
+        int b = nums2[0];
+
+        return Math.min(a * 10 + b, b * 10 + a);
     }
 }
