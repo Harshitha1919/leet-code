2cacd6b1ed1779e6c16de90a7924cf4b77eaad90
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2025-12-24 18:36:35 +0530

    Time: 7 ms (78.08%) | Memory: 46.5 MB (73.86%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -1,33 +1,25 @@
 class Solution {
     public int[] sortByBits(int[] arr) {
-        int count=0;
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            String binary=Integer.toBinaryString(arr[i]);
-              count=0;
-            for(int j=0;j<binary.length();j++){
-                if(binary.charAt(j)=='1'){
-                    count++;
-                }
-            }
-            hs.put(arr[i],count);
-          
 
+        Integer[] temp = new Integer[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            temp[i] = arr[i];
         }
-        for(int i=0;i<arr.length-1;i++){
-            for(int j=0;j<arr.length-1-i;j++){
-            if(hs.get(arr[j])>hs.get(arr[j+1])){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+
+        Arrays.sort(temp, (a, b) -> {
+            int bitA = Integer.bitCount(a);
+            int bitB = Integer.bitCount(b);
+
+            if (bitA != bitB) {
+                return bitA - bitB;      // sort by number of 1s
             }
-           else if((hs.get(arr[j]).equals(hs.get(arr[j+1]))) &&  arr[j]>arr[j+1]){
-                int temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
-            }
-         }
+            return a - b;               // if equal, sort by value
+        });
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = temp[i];
         }
+
         return arr;
     }
 }
