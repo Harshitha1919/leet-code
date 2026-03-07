0b1051b1672695d7a97a324597cd82434bddad6a
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-03-07 11:59:58 +0530

    Time: 9 ms (11.84%) | Memory: 46.8 MB (70.85%) - LeetSync

--- 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.java
+++ 4231-smallest-pair-with-different-frequencies/smallest-pair-with-different-frequencies.java
@@ -1,34 +1,31 @@
-import java.util.*;
-
 class Solution {
     public int[] minDistinctFreqPair(int[] nums) {
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-
-        // count frequencies
-        for(int num : nums){
-            map.put(num, map.getOrDefault(num,0) + 1);
+        HashMap<Integer,Integer> hs=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            hs.put(nums[i],hs.getOrDefault(nums[i],0)+1);
         }
-
-        // distinct values
-        ArrayList<Integer> arr = new ArrayList<>(map.keySet());
-
-        // sort values
-        Collections.sort(arr);
-
-        // find smallest x and smallest y
-        for(int i = 0; i < arr.size(); i++){
-            for(int j = i + 1; j < arr.size(); j++){
-
-                int x = arr.get(i);
-                int y = arr.get(j);
-
-                if(map.get(x) != map.get(y)){
-                    return new int[]{x,y};
-                }
+        ArrayList<Integer> arr=new ArrayList<>();
+        for(int i=0;i<nums.length;i++){
+            if(!arr.contains(nums[i])){
+                    arr.add(nums[i]);
             }
         }
+        Collections.sort(arr);
+        int x=0;
+        int y=0;
+        for(int i=0;i<arr.size();i++){
+            for(int j=i+1;j<arr.size();j++){
+            if(hs.get(arr.get(i))!=hs.get(arr.get(j))){
+                x=arr.get(i);
+                y=arr.get(j);
+                return new int[]{x,y};
+            }
+            }
+        }
+       
+        
+         return new int[]{-1,-1};
+        
 
-        return new int[]{-1,-1};
     }
 }
