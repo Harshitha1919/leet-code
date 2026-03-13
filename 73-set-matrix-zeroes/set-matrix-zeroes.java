f5dfce0e299e1b6e1cc79723657a95babb2d896b
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-03-13 21:09:40 +0530

    Time: 14 ms (3.91%) | Memory: 47.6 MB (15.52%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/set-matrix-zeroes.java
@@ -1,39 +1,30 @@
 class Solution {
     public void setZeroes(int[][] matrix) {
-
-        int m = matrix.length;
-        int n = matrix[0].length;
-
-        int[][] ans = new int[m][n];
-
-        // copy matrix
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+       int ans[][]=new int[matrix.length][matrix[0].length];
+         for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
                 ans[i][j] = matrix[i][j];
             }
         }
 
-        // process zeros
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==0){
-
-                    for(int k=0;k<m;k++){
-                        ans[k][j]=0;
-                    }
-
-                    for(int l=0;l<n;l++){
-                        ans[i][l]=0;
-                    }
+                   for(int k=0;k<matrix.length;k++){
+                      ans[k][j]=0;
+                   }
+                    for(int l=0;l<matrix[0].length;l++){
+                      ans[i][l]=0;
+                   }
                 }
+               
             }
         }
-
-        // copy back
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
+         for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
                 matrix[i][j] = ans[i][j];
             }
         }
+        
     }
 }
