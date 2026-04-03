1064f249bac507daadc7ff27745b96c8a5a6fad3
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-04-03 22:43:11 +0530

    Time: 1 ms (81.39%) | Memory: 42.4 MB (36.87%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.java
+++ 1448-maximum-69-number/maximum-69-number.java
@@ -4,7 +4,7 @@ class Solution {
         char[] arr = n.toCharArray();     
 
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == '6') {
+            if (arr[i] == '6'){
                 arr[i] = '9';   
                 break;
             }
