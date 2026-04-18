7f8e6c860e2dca5fb2358990cfafcfde49d4c510
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-04-18 19:19:04 +0530

    Time: 5 ms (27.53%) | Memory: 42.5 MB (90.55%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,29 +1,24 @@
-import java.util.*;
-
 class Solution {
-    int sum = 0;
-
-    private void subset(int i, int[] nums, List<Integer> arr) {
-        if (i == nums.length) {
-            int ans = 0;
-            for (int j = 0; j < arr.size(); j++) {
-                ans = ans ^ arr.get(j);
-            }
-            sum += ans;
-            return;  // ✅ VERY IMPORTANT
+    int sum=0;
+    private void subset(int i,int [] nums,List<Integer> arr){
+        if(i==nums.length){
+            int ans=0;
+             for(int j=0;j<arr.size();j++){
+                ans=ans^arr.get(j);
+             }
+             sum=sum+ans;
+         return ;
         }
-
-        // pick
         arr.add(nums[i]);
-        subset(i + 1, nums, arr);
-
-        // not pick
-        arr.remove(arr.size() - 1);
-        subset(i + 1, nums, arr);
+        subset(i+1,nums,arr);
+        arr.remove(arr.size()-1);
+        subset(i+1,nums,arr);
+      
     }
-
     public int subsetXORSum(int[] nums) {
-        subset(0, nums, new ArrayList<>());
-        return sum;
+        ArrayList<Integer> arr=new ArrayList<>();
+       subset(0,nums,arr);
+
+ return sum;
     }
 }
