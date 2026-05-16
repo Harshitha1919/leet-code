57992574a7c4a7e839fe708c83b55459da241e89
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-05-16 19:17:34 +0530

    Time: 5 ms (38.64%) | Memory: 44.9 MB (43.01%) - LeetSync

--- 2892-check-if-array-is-good/check-if-array-is-good.java
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -8,20 +8,14 @@ class Solution {
            }
       }
       int base[]=new int[max+1];
-      if(max==1){
-        base[0]=1;
-        base[1]=1;
-      } 
-     
-       
-      if(max>=2){
+    
         
         for(int i=0;i<max-1;i++){
              base[i]=i+1;
         }
         base[max-1]=max;
         base[max]=max;
-      } 
+      
       if(base.length!=nums.length){
         return false;
       }
