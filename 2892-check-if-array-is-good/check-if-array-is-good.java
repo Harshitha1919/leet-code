37a3695d651da2044e69f12de4df4845c72ce201
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-05-16 19:16:48 +0530

    Time: 5 ms (38.64%) | Memory: 45 MB (43.01%) - LeetSync

---
+++ 2892-check-if-array-is-good/check-if-array-is-good.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean isGood(int[] nums) {
+        int max=0;
+        int n=nums.length;
+      for(int i=0;i<nums.length;i++){
+           if(nums[i]>max){
+            max=nums[i];
+           }
+      }
+      int base[]=new int[max+1];
+      if(max==1){
+        base[0]=1;
+        base[1]=1;
+      } 
+      else if(max==2){
+        base[0]=1;
+        base[1]=max;
+        base[2]=max;
+      }
+       
+      if(max>=3){
+        
+        for(int i=0;i<max-1;i++){
+             base[i]=i+1;
+        }
+        base[max-1]=max;
+        base[max]=max;
+      } 
+      if(base.length!=nums.length){
+        return false;
+      }
+      Arrays.sort(base);
+      Arrays.sort(nums);
+      if(Arrays.equals(base,nums)){
+        return true;
+      }
+      return false;
+    }
+}
