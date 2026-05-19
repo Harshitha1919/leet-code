3d16dff8fa55ffe749496eb6a16c2f382b2ff897
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-05-19 23:57:15 +0530

    Time: 68 ms (5.03%) | Memory: 50.1 MB (53.20%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -2,40 +2,39 @@ class Solution {
     public int findShortestSubArray(int[] nums) {
 
         HashMap<Integer,Integer> hs = new HashMap<>();
-        int ans = nums.length;
-
+int ans=nums.length;
         for(int i = 0; i < nums.length; i++) {
             hs.put(nums[i], hs.getOrDefault(nums[i], 0) + 1);
         }
 
-        int maxFreq = 0;
+        int max =0;
 
         for(int n : hs.keySet()) {
-            maxFreq = Math.max(maxFreq, hs.get(n));
-        }
-
-        for(int n : hs.keySet()) {
-
-            if(hs.get(n) == maxFreq) {
-
-                int start = -1;
-                int end = -1;
-
-                for(int i = 0; i < nums.length; i++) {
-
-                    if(nums[i] == n && start == -1) {
-                        start = i;
-                    }
-
-                    if(nums[i] == n) {
-                        end = i;
-                    }
-                }
-
-                ans = Math.min(ans, end - start + 1);
+            if(hs.get(n) > max) {
+                max = hs.get(n);
             }
         }
+        for(int n : hs.keySet()) {
+               if(hs.get(n)==max){
+        int start = -1;
+        int end = -1;
 
-        return ans;
+        for(int i = 0; i < nums.length; i++) {
+         
+               
+
+            if(nums[i] == n && start == -1) {
+                start = i;
+            }
+
+             if(nums[i] == n) {
+                end = i;
+            }
+        }
+        ans=Math.min(ans,end-start+1);
+        }
+        }
+
+      return ans;
     }
 }
