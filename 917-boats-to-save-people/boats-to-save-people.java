5c52a917e3f736fd7122a9ddd023d9ed8843c280
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-07-26 11:55:13 +0530

    Time: 19 ms (98.62%) | Memory: 56.2 MB (95.19%) - LeetSync

--- 917-boats-to-save-people/boats-to-save-people.java
+++ 917-boats-to-save-people/boats-to-save-people.java
@@ -4,28 +4,13 @@ class Solution {
        int  sum=0;
        int i=0;
        int j=p.length-1;
-        HashMap<Integer,Integer> hs=new HashMap<>();
-       for( i=0;i<p.length;i++){
-           hs.put(p[i],hs.getOrDefault(p[i],0)+1);
-       }
-    i=0;
+      
+      
+  
        while(i<=j){
         if(p[i]+p[j]<=limit){
             sum++;
-           if(hs.containsKey(p[i])){
-             hs.put(p[i],hs.get(p[i])-1);
-             if(hs.get(p[i])==0){
-                hs.remove(p[i]);
-             }
 
-
-              if(hs.containsKey(p[j])){
-             hs.put(p[j],hs.get(p[j])-1);
-             if(hs.get(p[j])==0){
-                hs.remove(p[j]);
-             }
-           }
-           }
 
            
             i++;
@@ -33,15 +18,15 @@ class Solution {
             }
        else if(p[i]+p[j]>limit){
               j--;
+              sum++;
 
         }
         else{
             i++;
+
         }
        }
-       for(int h:hs.keySet()){
-              sum=sum+hs.get(h);
-       }
+     
 
        return sum;
     }
