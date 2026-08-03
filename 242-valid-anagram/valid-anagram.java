4d5f05055a0723df2a12255e9143a679cfac104a
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-08-03 14:54:26 +0530

    Time: 21 ms (7.48%) | Memory: 46.8 MB (5.15%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,13 +1,25 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
         HashMap<Character,Integer> hs=new HashMap<>();
-           HashMap<Character,Integer> hs1=new HashMap<>();
         for(int i=0;i<s.length();i++){
             hs.put(s.charAt(i),hs.getOrDefault(s.charAt(i),0)+1);
-        }
-        for(int i=0;i<t.length();i++){
-             hs1.put(t.charAt(i),hs1.getOrDefault(t.charAt(i),0)+1); 
-        }
-        return hs.equals(hs1);
+         }
+         for(int i=0;i<t.length();i++){
+            if(hs.containsKey(t.charAt(i))){
+                hs.put(t.charAt(i),hs.get(t.charAt(i))-1);
+                if(hs.get(t.charAt(i))==0){
+                    hs.remove(t.charAt(i));
+                }
+            }
+            else{
+
+                return false;
+            }
+            
+         }
+         return true;
     }
 }
