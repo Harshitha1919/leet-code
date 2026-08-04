f1a49312deff39352ebcc2bc0c2f31a6de1f248e
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-08-04 11:02:16 +0530

    Time: 3 ms (53.68%) | Memory: 46.4 MB (71.94%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.java
+++ 36-valid-sudoku/valid-sudoku.java
@@ -1,36 +1,53 @@
-
-
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        HashSet<Character>[] rows = new HashSet[9];
-        HashSet<Character>[] cols = new HashSet[9];
-        HashSet<Character>[] boxes = new HashSet[9];
-
-        for (int i = 0; i < 9; i++) {
-            rows[i] = new HashSet<>();
-            cols[i] = new HashSet<>();
-            boxes[i] = new HashSet<>();
-        }
-
-        for (int i = 0; i < 9; i++) {
-            for (int j = 0; j < 9; j++) {
-                if (board[i][j] == '.') {
-                    continue;
-                }
-
-                char ch = board[i][j];
-                int box = (i / 3) * 3 + (j / 3);
-
-                if (rows[i].contains(ch) || cols[j].contains(ch) || boxes[box].contains(ch)) {
+        HashSet<Character> hs=new HashSet<>();
+        HashSet<Character> hs1=new HashSet<>();
+           for(int i=0;i<9;i++){
+            hs.clear();
+            hs1.clear();
+              for(int j=0;j<9;j++){
+                if(board[i][j]!='.'){
+                if(hs.contains(board[i][j]) ){
                     return false;
                 }
+                
+                else{
+                    hs.add(board[i][j]);
+                }
+                }
 
-                rows[i].add(ch);
-                cols[j].add(ch);
-                boxes[box].add(ch);
+           if(board[j][i]!='.'){
+                if(hs1.contains(board[j][i])){
+                    return false;
+                }
+                
+                else{
+                    hs1.add(board[j][i]);
+                }
+           }
+              }
+           }
+
+          for (int row = 0; row < 9; row += 3) {
+    for (int col = 0; col < 9; col += 3) {
+
+        hs.clear();
+
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+
+                if (board[row + i][col + j] != '.') {
+
+                    if (hs.contains(board[row + i][col + j])) {
+                        return false;
+                    }
+
+                    hs.add(board[row + i][col + j]);
+                }
             }
         }
-
-        return true;
+    }
+}
+           return true;
     }
 }
