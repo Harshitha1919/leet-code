b715dbc6009db16930779a6fd75ed16dcc76640a
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-08-15 14:54:50 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (92.95%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -1,14 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
 
-        if (head == null || left == right) {
+         if (head == null || left == right) {
             return head;
         }
 
-        int count1 = 1;
-        ListNode temp = head;
-
-        if (left > 1) {
+        int count1=1;
+        ListNode temp=head;
+         if (left > 1) {
             while (count1 < left - 1) {
                 temp = temp.next;
                 count1++;
@@ -17,26 +26,27 @@ class Solution {
 
         ListNode start = (left == 1) ? head : temp.next;
         ListNode head2 = start;
-
-        int count2 = left;
-        ListNode prev = null;
-
-        while (count2 <= right) {
-            ListNode next = start.next;
-            start.next = prev;
-            prev = start;
-            start = next;
+ 
+        int count2=left;
+        ListNode prev=null;
+        while( count2<=right){
+           ListNode next=start.next;
+            start.next=prev;
+            prev=start;
+            start=next;
             count2++;
         }
-
-        if (left == 1) {
+         if (left == 1) {
             head2.next = start;
             return prev;
         }
 
-        temp.next = prev;
-        head2.next = start;
+        temp.next=prev;
+        head2.next=start;
+        
 
         return head;
+
+
     }
 }
