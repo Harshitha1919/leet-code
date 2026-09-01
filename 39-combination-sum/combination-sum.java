3406bf4ded1fc7317cd8f7ffee770d7f4a0db3f2
Harshitha1919 <147472951+Harshitha1919@users.noreply.github.com>
2026-09-01 14:53:00 +0530

    Time: 2 ms (95.50%) | Memory: 45.9 MB (33.43%) - LeetSync

--- 39-combination-sum/combination-sum.java
+++ 39-combination-sum/combination-sum.java
@@ -1,32 +1,39 @@
 class Solution {
-   int sum = 0;
+    int sum = 0;
     List<Integer> ans = new ArrayList<>();
     List<List<Integer>> arr = new ArrayList<>();
-    public void sum(int[] c,int target,int sum,int start,List<List<Integer>> arr){
-        if(sum==target){
+
+    public void sum(int[] c, int target, int sum, int start,
+                    List<List<Integer>> arr) {
+
+        if (sum == target) {
             arr.add(new ArrayList<>(ans));
             return;
         }
-           for(int i=start;i<c.length;i++){
-            if(sum+c[i]<=target){
-                sum=sum+c[i];
+
+        for (int i = start; i < c.length; i++) {
+
+            if (sum + c[i] < target) {
+                sum = sum + c[i];
                 ans.add(c[i]);
-                sum(c,target,sum,i,arr);
-                  ans.remove(ans.size()-1);
-                  sum=sum-c[i];
-          
-                
+
+                sum(c, target, sum, i, arr);
+
+                ans.remove(ans.size() - 1);
+                sum = sum - c[i];
             }
-          
-           
+            else if (sum + c[i] == target) {
+                ans.add(c[i]);
+                arr.add(new ArrayList<>(ans));
+                ans.remove(ans.size() - 1);
+            }
         }
     }
 
-    
     public List<List<Integer>> combinationSum(int[] c, int target) {
-      
-        sum(c,target,0,0,arr);
-        return arr;
 
+        sum(c, target, 0, 0, arr);
+
+        return arr;
     }
 }
